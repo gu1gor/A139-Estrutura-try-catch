9befff31b2e73b65594f4a084d15840c1e8695f6
Gustavo Igor <gustavo10fac_igor@outlook.com>
2021-07-15 15:39:58 -0300

    Projeto criado

---
+++ src/a139_estrutura_try_catch/A139_Estrutura_try_catch.java
@@ -0,0 +1,25 @@
+package a139_estrutura_try_catch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class A139_Estrutura_try_catch {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+        System.out.println("End of program");
+        sc.close();
+
+    }
+
+}
